f59ba70e219506d6a107ca4da15e8101f6301cf4
fm946969 <fm946969@hotmail.com>
2021-04-02 14:27:00 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
-def on_button_pressed_a():
-    controldevice(1, 255)
-    basic.pause(1000)
-    controldevice(2, 255)
-    basic.pause(1000)
-    controldevice(3, 255)
-    basic.pause(1000)
-    controldevice(4, 255)
-    basic.pause(5000)
-    controldevice(1, 0)
-    basic.pause(1000)
-    controldevice(2, 0)
-    basic.pause(1000)
-    controldevice(3, 0)
-    basic.pause(1000)
-    controldevice(4, 0)
-    basic.pause(1000)
-    controldevice(1, 255)
-    controldevice(2, 255)
-    controldevice(3, 255)
-    controldevice(4, 255)
-    basic.pause(5000)
-    controldevice(0, 0)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_received_number(receivedNumber):
+    if receivedNumber == 0:
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+        motor.motor_stop_all()
+    if receivedNumber % 10 != 0:
+        controldevice(1, 255)
+    else:
+        controldevice(1, 0)
+    if Math.floor(receivedNumber / 10) % 10 != 0:
+        controldevice(2, 255)
+    else:
+        controldevice(2, 0)
+    if Math.floor(receivedNumber / 100) % 10 != 0:
+        controldevice(3, 255)
+    else:
+        controldevice(3, 0)
+    if Math.floor(receivedNumber / 1000) % 10 != 0:
+        controldevice(4, 255)
+    else:
+        controldevice(4, 0)
+radio.on_received_number(on_received_number)
 
 def controldevice(id: number, power: number):
-    if power <= 0:
-        led.unplot(id, 0)
+    if power == 0:
+        led.unplot(id - 1, 0)
     else:
-        led.plot(id, 0)
-    if id == 0:
-        motor.motor_stop_all()
+        led.plot(id - 1, 0)
+    if power == 0:
+        if id == 1:
+            motor.motor_stop(motor.Motors.M1)
+        elif id == 2:
+            motor.motor_stop(motor.Motors.M2)
+        elif id == 3:
+            motor.motor_stop(motor.Motors.M3)
+        elif id == 4:
+            motor.motor_stop(motor.Motors.M4)
     elif id == 1:
         motor.motor_run(motor.Motors.M1, motor.Dir.CW, power)
     elif id == 2:
@@ -38,29 +48,6 @@ def controldevice(id: number, power: number):
         motor.motor_run(motor.Motors.M3, motor.Dir.CW, power)
     elif id == 4:
         motor.motor_run(motor.Motors.M4, motor.Dir.CW, power)
-
-def on_received_value(name, value):
-    if(name.compare("control") == 0):
-        if(value == 0):
-            motor.motor_stop_all()
-        if(value % 10 == 1):
-            controldevice(1, 255)
-        else:
-            controldevice(1, 0)
-        if((value / 10) % 10 == 1):
-            controldevice(2, 255)
-        else:
-            controldevice(2, 0)
-        if((value / 100) % 10 == 1):
-            controldevice(3, 255)
-        else:
-            controldevice(4, 0)
-        if((value / 1000) % 10 == 1):
-            controldevice(4, 255)
-        else:
-            controldevice(4, 0)
-radio.on_received_value(on_received_value)
-
-text_list = ["Off", "Led", "Pump1", "Pump2", "Pump3"]
 motor.motor_stop_all()
+radio.set_transmit_power(7)
 radio.set_group(66)
--- main.ts
+++ main.ts
@@ -1,11 +1,51 @@
-function controldevice (id: number, power: number) {
-    if (power <= 0) {
-        led.unplot(id, 0)
-    } else {
-        led.plot(id, 0)
-    }
-    if (id == 0) {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
         motor.motorStopAll()
+    }
+    if (receivedNumber % 10 != 0) {
+        controldevice(1, 255)
+    } else {
+        controldevice(1, 0)
+    }
+    if (Math.floor(receivedNumber / 10) % 10 != 0) {
+        controldevice(2, 255)
+    } else {
+        controldevice(2, 0)
+    }
+    if (Math.floor(receivedNumber / 100) % 10 != 0) {
+        controldevice(3, 255)
+    } else {
+        controldevice(3, 0)
+    }
+    if (Math.floor(receivedNumber / 1000) % 10 != 0) {
+        controldevice(4, 255)
+    } else {
+        controldevice(4, 0)
+    }
+})
+function controldevice (id: number, power: number) {
+    if (power == 0) {
+        led.unplot(id - 1, 0)
+    } else {
+        led.plot(id - 1, 0)
+    }
+    if (power == 0) {
+        if (id == 1) {
+            motor.motorStop(motor.Motors.M1)
+        } else if (id == 2) {
+            motor.motorStop(motor.Motors.M2)
+        } else if (id == 3) {
+            motor.motorStop(motor.Motors.M3)
+        } else if (id == 4) {
+            motor.motorStop(motor.Motors.M4)
+        }
     } else if (id == 1) {
         motor.MotorRun(motor.Motors.M1, motor.Dir.CW, power)
     } else if (id == 2) {
@@ -16,32 +56,6 @@ function controldevice (id: number, power: number) {
         motor.MotorRun(motor.Motors.M4, motor.Dir.CW, power)
     }
 }
-radio.onReceivedValue(function (name, value) {
-    if (name.compare("control") == 0) {
-        if (value == 0) {
-            motor.motorStopAll()
-        }
-        if (value % 10 == 1) {
-            controldevice(1, 255)
-        } else {
-            controldevice(1, 0)
-        }
-        if (value / 10 % 10 == 1) {
-            controldevice(2, 255)
-        } else {
-            controldevice(2, 0)
-        }
-        if (value / 100 % 10 == 1) {
-            controldevice(3, 255)
-        } else {
-            controldevice(4, 0)
-        }
-        if (value / 1000 % 10 == 1) {
-            controldevice(4, 255)
-        } else {
-            controldevice(4, 0)
-        }
-    }
-})
 motor.motorStopAll()
+radio.setTransmitPower(7)
 radio.setGroup(66)
