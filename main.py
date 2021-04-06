02794f31e82cb6201742cd1d6fc0085b4bbbe665
fm946969 <fm946969@hotmail.com>
2021-04-06 11:23:58 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -8,46 +8,52 @@ def on_received_number(receivedNumber):
             . . . . .
             """)
         motor.motor_stop_all()
-    if receivedNumber % 10 != 0:
-        controldevice(1, 255)
     else:
-        controldevice(1, 0)
-    if Math.floor(receivedNumber / 10) % 10 != 0:
-        controldevice(2, 255)
-    else:
-        controldevice(2, 0)
-    if Math.floor(receivedNumber / 100) % 10 != 0:
-        controldevice(3, 255)
-    else:
-        controldevice(3, 0)
-    if Math.floor(receivedNumber / 1000) % 10 != 0:
-        controldevice(4, 255)
-    else:
-        controldevice(4, 0)
+        for i in range(4):
+            parsecode(i, receivedNumber)
 radio.on_received_number(on_received_number)
 
 def controldevice(id: number, power: number):
-    if power == 0:
-        led.unplot(id - 1, 0)
-    else:
-        led.plot(id - 1, 0)
-    if power == 0:
-        if id == 1:
-            motor.motor_stop(motor.Motors.M1)
+    global actual_power
+    actual_power = max(power, list_power_min[id])
+    if list_power[id] != actual_power:
+        list_power[id] = actual_power
+        led.plot_brightness(id, 0, actual_power)
+        if actual_power == 0:
+            if id == 0:
+                motor.motor_stop(motor.Motors.M1)
+            elif id == 1:
+                motor.motor_stop(motor.Motors.M2)
+            elif id == 2:
+                motor.motor_stop(motor.Motors.M3)
+            elif id == 3:
+                motor.motor_stop(motor.Motors.M4)
+        elif id == 0:
+            motor.motor_run(motor.Motors.M1, motor.Dir.CW, actual_power)
+        elif id == 1:
+            motor.motor_run(motor.Motors.M2, motor.Dir.CW, actual_power)
         elif id == 2:
-            motor.motor_stop(motor.Motors.M2)
+            motor.motor_run(motor.Motors.M3, motor.Dir.CW, actual_power)
         elif id == 3:
-            motor.motor_stop(motor.Motors.M3)
-        elif id == 4:
-            motor.motor_stop(motor.Motors.M4)
-    elif id == 1:
-        motor.motor_run(motor.Motors.M1, motor.Dir.CW, power)
-    elif id == 2:
-        motor.motor_run(motor.Motors.M2, motor.Dir.CW, power)
-    elif id == 3:
-        motor.motor_run(motor.Motors.M3, motor.Dir.CW, power)
-    elif id == 4:
-        motor.motor_run(motor.Motors.M4, motor.Dir.CW, power)
+            motor.motor_run(motor.Motors.M4, motor.Dir.CW, actual_power)
+def parsecode(id: number, code: number):
+    if Math.floor(code / 10 ** id) % 10 != 0:
+        controldevice(id, 255)
+    else:
+        controldevice(id, 0)
+
+def on_button_pressed_a():
+    list2: List[number] = []
+    list_power_min[3] = 255 - list2[3]
+    controldevice(3, list2[3])
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+actual_power = 0
+list_power_min: List[number] = []
+list_power: List[number] = []
+Pump3MinPower = 0
 motor.motor_stop_all()
 radio.set_transmit_power(7)
 radio.set_group(66)
+list_power = [0, 0, 0, 0]
+list_power_min = [0, 0, 0, 0]
--- main.ts
+++ main.ts
@@ -8,54 +8,62 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
         motor.motorStopAll()
-    }
-    if (receivedNumber % 10 != 0) {
-        controldevice(1, 255)
     } else {
-        controldevice(1, 0)
-    }
-    if (Math.floor(receivedNumber / 10) % 10 != 0) {
-        controldevice(2, 255)
-    } else {
-        controldevice(2, 0)
-    }
-    if (Math.floor(receivedNumber / 100) % 10 != 0) {
-        controldevice(3, 255)
-    } else {
-        controldevice(3, 0)
-    }
-    if (Math.floor(receivedNumber / 1000) % 10 != 0) {
-        controldevice(4, 255)
-    } else {
-        controldevice(4, 0)
+        for (let i = 0; i <= 3; i++) {
+            parsecode(i, receivedNumber)
+        }
     }
 })
 function controldevice (id: number, power: number) {
-    if (power == 0) {
-        led.unplot(id - 1, 0)
-    } else {
-        led.plot(id - 1, 0)
-    }
-    if (power == 0) {
-        if (id == 1) {
-            motor.motorStop(motor.Motors.M1)
+    actual_power = Math.max(power, list_power_min[id])
+    if (list_power[id] != actual_power) {
+        list_power[id] = actual_power
+        led.plotBrightness(id, 0, actual_power)
+        if (actual_power == 0) {
+            if (id == 0) {
+                motor.motorStop(motor.Motors.M1)
+            } else if (id == 1) {
+                motor.motorStop(motor.Motors.M2)
+            } else if (id == 2) {
+                motor.motorStop(motor.Motors.M3)
+            } else if (id == 3) {
+                motor.motorStop(motor.Motors.M4)
+            }
+        } else if (id == 0) {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, actual_power)
+        } else if (id == 1) {
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, actual_power)
         } else if (id == 2) {
-            motor.motorStop(motor.Motors.M2)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, actual_power)
         } else if (id == 3) {
-            motor.motorStop(motor.Motors.M3)
-        } else if (id == 4) {
-            motor.motorStop(motor.Motors.M4)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CW, actual_power)
         }
-    } else if (id == 1) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, power)
-    } else if (id == 2) {
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, power)
-    } else if (id == 3) {
-        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, power)
-    } else if (id == 4) {
-        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, power)
     }
 }
+function parsecode (id: number, code: number) {
+    if (Math.floor(code / 10 ** id) % 10 != 0) {
+        controldevice(id, 255)
+    } else {
+        controldevice(id, 0)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    toggle_min_power(0)
+})
+input.onButtonPressed(Button.B, function () {
+    toggle_min_power(3)
+})
+function toggle_min_power (id: number) {
+    list_power_min[id] = 255 - list_power_min[id]
+    led.plotBrightness(id, 1, list_power_min[id])
+    controldevice(id, list_power_min[id])
+}
+let actual_power = 0
+let list_power_min: number[] = []
+let list_power: number[] = []
+let Pump3MinPower = 0
 motor.motorStopAll()
 radio.setTransmitPower(7)
 radio.setGroup(66)
+list_power = [0, 0, 0, 0]
+list_power_min = [0, 0, 0, 0]
